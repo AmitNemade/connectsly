5da5723b3ed4b95d247e1e91639662dddff30b0a
AmitNemade <amitnemade34@gmail.com>
2020-09-01 17:25:00 +0530

    heroku v1.0

--- src/Video.js
+++ src/Video.js
@@ -19,7 +19,7 @@ import Modal from 'react-bootstrap/Modal'
 import 'bootstrap/dist/css/bootstrap.css'
 import "./Video.css"
 
-const server_url = process.env.NODE_ENV === 'production' ? 'https://clone-zoom-1.herokuapp.com/' : "http://localhost:4001"
+const server_url = process.env.NODE_ENV === 'production' ? 'https://connectsly.herokuapp.com/' : "http://localhost:4001"
 
 var connections = {}
 var dataChannel = {}
